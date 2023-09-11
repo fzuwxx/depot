0c271a02fd550fbbbe0c6adf0fc8a1ca194f7f98
fzuwxx <128562577+fzuwxx@users.noreply.github.com>
2023-09-11 21:11:09 +0800

    通过网址爬取弹幕

---
+++ 102101518/get_data.py
@@ -0,0 +1,22 @@
+import requests
+import re
+import chardet
+
+'''爬取弹幕'''
+
+
+def get_data(cid):
+
+    # https://api.bilibili.com/x/v1/dm/list.so?oid=XXX
+    # 最终爬取API：https://api.bilibili.com/x/v1/dm/list.so?oid=?
+    try:
+        final_url = "https://api.bilibili.com/x/v1/dm/list.so?oid="+str(cid)
+        final_res = requests.get(final_url)
+        final_res.encoding = 'utf-8'
+        final_res_encoding = chardet.detect(final_res.content)
+        final_res = final_res.text
+        pattern = re.compile('<d.*?>(.*?)</d>')
+        data = pattern.findall(final_res)
+        return data
+    except Exception as e:
+        print("执行get_data失败：", e )
