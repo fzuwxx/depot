0e80d1ccce78ec4fa442f0eed71ec6d740781d19
fzuwxx <128562577+fzuwxx@users.noreply.github.com>
2023-09-12 16:56:09 +0800

    将弹幕数据写入csv文件（最终版）

--- 102101518/save_to_file.py
+++ 102101518/save_to_file.py
@@ -1,12 +1,14 @@
-'''将弹幕数据写入excel'''
+import csv
+
+'''将数据写入csv'''
 
 
 def save_to_file(data):
     try:
-        with open('danmu_data.xlsx', mode="a", encoding='utf-8') as f:
-            for i in data:
-                f.write(i)
-                f.write("\n")
-        f.close()
+        with open('danmu_data.csv', 'a', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            for d in data:
+                writer.writerow([d])
+        print("数据保存成功！")
     except Exception as e:
         print("执行保存文件报错：", e)
